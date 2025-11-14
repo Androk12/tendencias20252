f8920aac80478b8342577a27a44dc07fe05055f3
Diego Ocampo <117787259+Diegoocamp70@users.noreply.github.com>
2025-11-14 17:35:04 -0500

    cambio en en url de registro

--- frontend/src/pages/Registro.jsx
+++ frontend/src/pages/Registro.jsx
@@ -26,7 +26,7 @@ const Registro = () => {
     }
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/registro/", {
+      const response = await fetch("https://tendencias20252-td7g.onrender.com/registro/", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
